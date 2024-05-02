b717cbbdc1fce88929bf5a2baa37dfcbb7c533b5
Md Shahriyar Alam <mdshahriyaralam552@gmail.com>
2024-05-02 11:25:46 +0600

    added login instruction

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,32 +1,44 @@
-import LoginForm from "@/components/LoginForm"
-import { Flex } from "antd"
-import React from "react"
+import LoginForm from "@/components/LoginForm";
+import { Flex } from "antd";
+import React from "react";
 
 const Home = () => (
-	<div className="App">
-		<div
-			className="container"
-			style={{
-				paddingBlock: "20px",
-			}}
-		>
-			<h2 style={{ fontSize: "40px", fontWeight: "bold", textAlign: "center" }}>
-				Task Management Login
-			</h2>
-		</div>
+  <div className="App">
+    <div
+      className="container"
+      style={{
+        paddingBlock: "20px",
+      }}
+    >
+      <h2 style={{ fontSize: "40px", fontWeight: "bold", textAlign: "center" }}>
+        Task Management Login
+      </h2>
+    </div>
 
-		<div
-			style={{
-				width: "100%",
-				marginTop: "100px",
-				padding: "0",
-				marginInline: "auto",
-        maxWidth: '600px'
-			}}
-		>
-			<LoginForm />
-		</div>
-	</div>
-)
+    <div
+      style={{
+        width: "100%",
+        marginTop: "100px",
+        padding: "0",
+        marginInline: "auto",
+        maxWidth: "600px",
+      }}
+    >
+      <LoginForm />
+    </div>
 
-export default Home
+    <p className="text-center">
+			<div>
+				<b>Username: </b>
+				<span>admin</span>
+			</div>
+
+			<div>
+				<b>Password: </b>
+				<span>password</span>
+			</div>
+    </p>
+  </div>
+);
+
+export default Home;
